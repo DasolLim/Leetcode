21132af209f2f1314965bf6ac67e88bcbbd680dd
Dasol Lim <dlim67@uwo.ca>
2024-10-25 19:57:52 -0400

    Leetcode problem MoveZeros

---
+++ TwoPointers/MoveZeros.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        l = 0 
+        # use right to iterate
+        for r in range(len(nums)):
+            if nums[r]:
+                #swap
+                nums [l], nums[r] = nums[r], nums[l]
+                # increment
+                l += 1
+        return nums
+
+        
